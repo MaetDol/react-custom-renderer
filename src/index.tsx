6b77da4fd7ba4be481da6cb401176d098745aa3a
MaetDol <rndka9@gmail.com>
2022-09-19 20:47:09 +0900

    타입 가드 분리 및 렌더러를 통해 그리게 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,63 +1,117 @@
-import React, { Children, PropsWithChildren, ReactNode } from "react";
+import type {
+  FunctionComponent,
+  PropsWithChildren,
+  ReactElement,
+  ReactNode,
+} from "react";
+import React, { Children } from "react";
 import { App } from "./components";
+import { isFragment, isHTMLElement, isReactElement } from "./type-gaurd/react";
 
-function isHTMLElement(
-  component: ReactNode
-): component is React.ReactElement<PropsWithChildren> {
-  return !!component && typeof component["type"] === "string";
-}
+type NodeInfo<T> = {
+  node: T;
+  name: string;
+  props: PropsWithChildren;
+};
 
-function isFragment(
-  component: ReactNode
-): component is React.ReactElement<PropsWithChildren> {
-  return !!component && component["type"] === React.Fragment;
-}
+type RendererBase<T> = (node: NodeInfo<T>, depth: number) => void;
 
-function isReactElement(
-  component: ReactNode
-): component is React.ReactElement<PropsWithChildren, React.FunctionComponent> {
-  return !!component && typeof component["type"] === "function";
-}
+type RenderSet = Partial<{
+  default: RendererBase<ReactNode>;
+  falsy: RendererBase<undefined | null | boolean>;
+  primitive: RendererBase<string | number>;
+  html: RendererBase<ReactElement<PropsWithChildren>>;
+  fragment: RendererBase<ReactElement<PropsWithChildren>>;
+  component: RendererBase<ReactElement<PropsWithChildren, FunctionComponent>>;
+}>;
 
-function render(node: ReactNode, indent = "") {
-  const indented = indent + "  ";
+function componentDepthFirstSearch(
+  node: ReactNode,
+  render?: RenderSet,
+  depth = 0
+) {
+  const useRender = <T,>(renderer?: RendererBase<T>) =>
+    renderer ?? render?.default;
+  const nextDepth = depth + 1;
 
   // Falsy value
-  if (node === null) {
-    console.log(`${indent}Um... falsy? ${node}`);
+  // 타는 경우가 없는 것 같기도?
+  if (node === undefined || node === null || typeof node === "boolean") {
+    useRender(render?.falsy)?.(
+      {
+        node,
+        name: typeof node,
+        props: {},
+      },
+      nextDepth
+    );
     return;
   }
 
   // Primitive literal
   if (typeof node === "string" || typeof node === "number") {
-    console.log(`${indent}string - ${node}`);
+    useRender(render?.primitive)?.(
+      {
+        node,
+        name: typeof node,
+        props: {},
+      },
+      nextDepth
+    );
     return;
   }
 
   // HTMLElement
   if (isHTMLElement(node)) {
-    console.log(indent + node.type);
+    useRender(render?.html)?.(
+      {
+        node,
+        name: node.type.toString(),
+        props: node.props,
+      },
+      depth
+    );
     Children.toArray(node.props.children).forEach((child) =>
-      render(child, indented)
+      componentDepthFirstSearch(child, render, nextDepth)
     );
     return;
   }
 
   // Fragment
   if (isFragment(node)) {
-    console.log(indent + "Fragment");
+    useRender(render?.fragment)?.(
+      {
+        node,
+        name: "Fragment",
+        props: node.props,
+      },
+      depth
+    );
     Children.toArray(node.props.children).forEach((child) =>
-      render(child, indented)
+      componentDepthFirstSearch(child, render, nextDepth)
     );
     return;
   }
 
   // Custom Component
   if (isReactElement(node)) {
-    console.log(indent + node.type.name);
-    render(node.type(node.props), indented);
+    useRender(render?.component)?.(
+      {
+        node,
+        name: node.type.name,
+        props: node.props,
+      },
+      depth
+    );
+    componentDepthFirstSearch(node.type(node.props), render, nextDepth);
     return;
   }
 }
 
-render(<App />);
+const consoleRenderer: RenderSet = {
+  default: (node, depth) => {
+    console.log("  ".repeat(depth) + node.name);
+  },
+};
+
+componentDepthFirstSearch(<App />, consoleRenderer);
