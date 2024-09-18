739afc2941fc4ddb32396aa91783c4f322487f32
MaetDol <rndka9@gmail.com>
2024-09-18 11:02:03 +0900

    bugfix, 타입 가드 타입 에러 수정

--- src/type-gaurd/react.ts
+++ src/type-gaurd/react.ts
@@ -6,20 +6,34 @@ import type {
 } from "react";
 import { Fragment } from "react";
 
+function getComponentType(comp: ReactNode) {
+  if (comp === null) return "null";
+  if (comp === undefined) return "undefined";
+  if (typeof comp === "number") return "number";
+  if (typeof comp === "string") return "string";
+  if (typeof comp === "boolean") return "boolean";
+  if (Symbol.iterator in comp) return "fragment";
+  if (comp.type === Fragment) return "fragment";
+  if (typeof comp.type === "string") return "html";
+  if (typeof comp.type === "function") return "react";
+
+  return "unknown";
+}
+
 export function isHTMLElement(
   component: ReactNode
 ): component is ReactElement<PropsWithChildren> {
-  return !!component && typeof component["type"] === "string";
+  return getComponentType(component) === "html";
 }
 
 export function isFragment(
   component: ReactNode
 ): component is ReactElement<PropsWithChildren> {
-  return !!component && component["type"] === Fragment;
+  return getComponentType(component) === "fragment";
 }
 
 export function isReactElement(
   component: ReactNode
 ): component is ReactElement<PropsWithChildren, FunctionComponent> {
-  return !!component && typeof component["type"] === "function";
+  return getComponentType(component) === "react";
 }
